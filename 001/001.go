9d1a0b8ab439070261dead754bd8d8247270e7ce
lcdmast <1479720976@qq.com>
2021-12-06 21:24:04 +0800

    git@github.com:lcdmast/mygo.git

--- 001/001.go
+++ 001/001.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Printf("my name is %v age %v", "lcd", "18")
-
+	var command = "li chang dong"
+	var exit = strings.Contains(command, "li")
+	fmt.Println("you leave", exit)
 }
