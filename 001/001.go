1f587776f384f6e6a66afeaf7102756882668f59
lcdmast <1479720976@qq.com>
2021-12-07 08:33:45 +0800

    werwe

--- 001/001.go
+++ 001/001.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
 )
 
@@ -10,4 +11,28 @@ func main() {
 	var command = "li chang dong"
 	var exit = strings.Contains(command, "li")
 	fmt.Println("you leave", exit)
+
+	var room = "lake"
+	switch {
+	case room == "cave":
+		fmt.Println("1111")
+	case room == "lake":
+		fmt.Println("222")
+		// fallthrough
+	case room == "lake3":
+		fmt.Println("33")
+	}
+	for count := 10; count > 0; count-- {
+		fmt.Println(count)
+	}
+
+	if num := rand.Intn(10); num == 0 {
+		fmt.Println("11")
+	} else if num == 1 {
+		fmt.Println(num)
+	} else {
+		fmt.Println("33")
+	}
+
+	fmt.Println(rand.Intn(5))
 }
